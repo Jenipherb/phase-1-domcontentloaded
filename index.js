172ee6c794d5431d0c20b3e9268512f3b4701c9d
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-06-29 15:26:47 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+//const text = document.getElementById('text')
+
+//textContent.addEventListener('DOMContentLoaded', () => {
+    //return text = "This is really cool!"
+
+
+//});
+
+//document.addEventListener('DOMContentLoaded', () =>{
+    //document.getElementById('text'), textConent.innerHTML = ("This is really cool!")
+   //return "This is really cool!"
+    //console.log ("This is really cool!")
+//}
+//);
+document.addEventListener("DOMContentLoaded", () => {
+    //document.getElementById('text');
+    const cool = document.getElementById('text');
+    cool.innerHTML = ("This is really cool!");
+});
